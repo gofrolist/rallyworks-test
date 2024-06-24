e35d13cac18910a5c17f4ab419915a0fc66135eb
Evgenii Vasilenko <evgenii_vasilenko@apple.com>
2024-06-24 12:34:55 -0500

    fix subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,23 +94,31 @@ module "subnets" {
   source  = "cloudposse/dynamic-subnets/aws"
   version = "2.4.2"
 
-  availability_zones              = var.availability_zones
-  vpc_id                          = module.vpc.vpc_id
-  igw_id                          = [module.vpc.igw_id]
-  ipv4_cidr_block                 = [module.vpc.vpc_cidr_block]
-  ipv6_cidr_block                 = [module.vpc.vpc_ipv6_cidr_block]
-  ipv6_enabled                    = true
-  max_nats                        = 1
-  nat_gateway_enabled             = true
-  nat_instance_enabled            = false
-  tags                            = merge(
+  availability_zones      = var.availability_zones
+  vpc_id                  = module.vpc.vpc_id
+  igw_id                  = [module.vpc.igw_id]
+  ipv4_enabled            = true
+  ipv6_enabled            = true
+  ipv6_egress_only_igw_id = [module.vpc.ipv6_egress_only_igw_id]
+  ipv4_cidr_block         = [module.vpc.vpc_cidr_block]
+  ipv6_cidr_block         = [module.vpc.vpc_ipv6_cidr_block]
+  nat_gateway_enabled     = true
+  nat_instance_enabled    = false
+  route_create_timeout    = "5m"
+  route_delete_timeout    = "10m"
+
+  subnet_type_tag_key = "cpco.io/subnet/type"
+
+  subnets_per_az_count = var.subnets_per_az_count
+  subnets_per_az_names = var.subnets_per_az_names
+
+  private_open_network_acl_enabled = var.default_nacls_enabled
+  public_open_network_acl_enabled  = var.default_nacls_enabled
+
+  tags = merge(
     local.tags,
     module.label.tags
   )
-  public_subnets_additional_tags  = local.public_subnets_additional_tags
-  private_subnets_enabled         = true
-  private_subnets_additional_tags = local.private_subnets_additional_tags
-
   context = module.this.context
 }
 
